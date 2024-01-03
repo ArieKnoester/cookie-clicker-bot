7c42f71e2dc0c96318d489a1e9f6c115d96fe537
Arie Knoester <arikstr@gmail.com>
2024-01-03 15:50:23 -0600

    Selects English as the language preference.

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 # https://www.selenium.dev/documentation/
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.expected_conditions import element_to_be_clickable
 
 
 driver = webdriver.Firefox()
 driver.get("https://orteil.dashnet.org/cookieclicker/")
 
-
+# When the browser first opens a prompt will display asking the user for a language preference.
+# However, this prompt does not display immediately. The line below forces selenium to wait
+# (up to 10 secs) for the "English' button to be clickable.
+WebDriverWait(driver, timeout=10).until(element_to_be_clickable((By.CSS_SELECTOR, "#langSelect-EN"))).click()
 
 driver.quit()
