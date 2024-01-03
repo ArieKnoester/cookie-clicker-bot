7bc361dbdfce40cf82eece9be43b6c01622a8439
Arie Knoester <arikstr@gmail.com>
2024-01-03 15:51:47 -0600

    Resolves ElementClickInterceptedException.

--- main.py
+++ main.py
@@ -13,4 +13,9 @@ driver.get("https://orteil.dashnet.org/cookieclicker/")
 # (up to 10 secs) for the "English' button to be clickable.
 WebDriverWait(driver, timeout=10).until(element_to_be_clickable((By.CSS_SELECTOR, "#langSelect-EN"))).click()
 
+# We also need to wait until the cookie is clickable. Error message:
+# ElementClickInterceptedException: Message: Element <button id="bigCookie"> is not clickable
+# at point (192,234) because another element <div id="loader"> obscures it
+WebDriverWait(driver, timeout=10).until(element_to_be_clickable((By.ID, "bigCookie")))
+
 driver.quit()
