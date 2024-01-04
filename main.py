3cfe294c8c1bbeddbedc0dfdb6a7ba5addff9953
Arie Knoester <arikstr@gmail.com>
2024-01-04 15:31:37 -0600

    Adds a 2 sec wait time so accept cookies button can be clicked.

--- main.py
+++ main.py
@@ -3,20 +3,22 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support.expected_conditions import element_to_be_clickable
-
+from selenium.webdriver.support.expected_conditions import element_to_be_clickable, invisibility_of_element
+import time
 
 driver = webdriver.Firefox()
 driver.get("https://orteil.dashnet.org/cookieclicker/")
 
 
+# Wait 2 secs, otherwise a <p class="cc_message">
+# will obscure the accept cookies button
+time.sleep(2)
+
 # We need to click the accept cookies button, otherwise this bot will
 # not be able to click on products in the 'Buildings' panel.
 WebDriverWait(driver, timeout=10).until(element_to_be_clickable((By.CSS_SELECTOR, ".cc_btn.cc_btn_accept_all"))).click()
 
-# When the browser first opens a prompt will display asking the user for a language preference.
-# However, this prompt does not display immediately. The line below forces selenium to wait
-# (up to 10 secs) for the "English' button to be clickable.
+# A prompt will display asking the user for a language preference.
 WebDriverWait(driver, timeout=10).until(element_to_be_clickable((By.CSS_SELECTOR, "#langSelect-EN"))).click()
 
 # Once a language is selected the site will reload. We need to wait until the cookie is clickable.
@@ -29,20 +31,24 @@ cookie = driver.find_element(By.ID, value="bigCookie")
 while True:
     cookie.click()
 
-    cookies_per_minute = int(driver.find_element(By.ID, value="cookies").text.split(" ")[0])
-    # print(cookies_per_minute)
+    current_cookie_count = int(driver.find_element(By.ID, value="cookies").text.split(" ")[0])
+    # print(current_cookie_count)
 
-    products = driver.find_elements(By.CSS_SELECTOR, value=".product.unlocked.enabled")
+    available_products = driver.find_elements(By.CSS_SELECTOR, value=".product.unlocked.enabled")
     product_prices = []
-    for product in products:
+    for product in available_products:
         # print(product.text.replace("\n", " ").split(" ")[1])
         product_prices.append(int(product.text.replace("\n", " ").split(" ")[1]))
 
     if product_prices:
         print(product_prices)
-        max_price_product_position = product_prices.index(max(product_prices))
-        print(max_price_product_position)
-        WebDriverWait(driver, timeout=10).until(element_to_be_clickable((By.ID, f"product{max_price_product_position}"))).click()
+        max_price = max(product_prices)
+        if max_price < current_cookie_count:
+            max_price_product_position = product_prices.index(max_price)
+            print(f"current_cookie_count: {current_cookie_count}, max_price: {max_price}")
+        else:
+            product_prices.pop(max_price)
+        # WebDriverWait(driver, timeout=10).until(element_to_be_clickable((By.ID, f"product{max_price_product_position}"))).click()
         # product_to_buy = driver.find_element(By.ID, value=f"product{max_price_product_position}")
         # print(product_to_buy.text)
         # product_to_buy.click()
