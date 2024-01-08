1c7c9b7ff139f302e1c7112ed63dc2cf1736640f
Arie Knoester <arikstr@gmail.com>
2024-01-08 14:46:58 -0600

    Cookie click loop stops after 5 minutes.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support.expected_conditions import element_to_be_clickable, invisibility_of_element
+from datetime import timedelta
 import time
 
 driver = webdriver.Firefox()
@@ -28,26 +29,31 @@ WebDriverWait(driver, timeout=10).until(element_to_be_clickable((By.CSS_SELECTOR
 WebDriverWait(driver, timeout=10).until(element_to_be_clickable((By.ID, "bigCookie")))
 
 cookie = driver.find_element(By.ID, value="bigCookie")
+timeout = time.time() + (60 * 5)  # 60 sec * 5 =  5 minutes
+
 while True:
+    # Run this loop for 5 minutes.
+    if time.time() > timeout:
+        break
     cookie.click()
 
     current_cookie_count = int(driver.find_element(By.ID, value="cookies").text.split(" ")[0])
     # print(current_cookie_count)
 
-    available_products = driver.find_elements(By.CSS_SELECTOR, value=".product.unlocked.enabled")
-    product_prices = []
-    for product in available_products:
-        # print(product.text.replace("\n", " ").split(" ")[1])
-        product_prices.append(int(product.text.replace("\n", " ").split(" ")[1]))
-
-    if product_prices:
-        print(product_prices)
-        max_price = max(product_prices)
-        if max_price < current_cookie_count:
-            max_price_product_position = product_prices.index(max_price)
-            print(f"current_cookie_count: {current_cookie_count}, max_price: {max_price}")
-        else:
-            product_prices.pop(max_price)
+    # available_products = driver.find_elements(By.CSS_SELECTOR, value=".product.unlocked.enabled")
+    # product_prices = []
+    # for product in available_products:
+    #     # print(product.text.replace("\n", " ").split(" ")[1])
+    #     product_prices.append(int(product.text.replace("\n", " ").split(" ")[1]))
+    #
+    # if product_prices:
+    #     print(product_prices)
+    #     max_price = max(product_prices)
+    #     if max_price <= current_cookie_count:
+    #         max_price_product_position = product_prices.index(max_price)
+    #         print(f"current_cookie_count: {current_cookie_count}, max_price: {max_price}")
+    #     else:
+    #         product_prices.pop(max_price)
         # WebDriverWait(driver, timeout=10).until(element_to_be_clickable((By.ID, f"product{max_price_product_position}"))).click()
         # product_to_buy = driver.find_element(By.ID, value=f"product{max_price_product_position}")
         # print(product_to_buy.text)
