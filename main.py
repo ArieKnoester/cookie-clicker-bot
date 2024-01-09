ff14452384fc346f08acbb3f1a51215c5a4138c9
Arie Knoester <arikstr@gmail.com>
2024-01-09 13:17:10 -0600

    Function to check products available called every 5 seconds.

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 # https://www.selenium.dev/documentation/
+import threading
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support.expected_conditions import element_to_be_clickable, invisibility_of_element
-from datetime import timedelta
+from datetime import datetime, timedelta
 import time
 
-driver = webdriver.Firefox()
-driver.get("https://orteil.dashnet.org/cookieclicker/")
+SITE_URL = "https://orteil.dashnet.org/cookieclicker/"
+PRODUCT_CHECK_INTERVAL = timedelta(seconds=5)
 
+
+def check_products(*, dr, cookie_count):
+    print('check products executed @', time.asctime())
+
+
+driver = webdriver.Firefox()
+driver.get(SITE_URL)
 
 # Wait 2 secs, otherwise a <p class="cc_message">
 # will obscure the accept cookies button
@@ -30,14 +39,16 @@ WebDriverWait(driver, timeout=10).until(element_to_be_clickable((By.ID, "bigCook
 
 cookie = driver.find_element(By.ID, value="bigCookie")
 timeout = time.time() + (60 * 5)  # 60 sec * 5 =  5 minutes
+last_time_products_checked = datetime.now()
 
-while True:
-    # Run this loop for 5 minutes.
-    if time.time() > timeout:
-        break
+while time.time() < timeout:
     cookie.click()
-
     current_cookie_count = int(driver.find_element(By.ID, value="cookies").text.split(" ")[0])
+
+    if last_time_products_checked <= (datetime.now() - PRODUCT_CHECK_INTERVAL):
+        check_products(dr=driver, cookie_count=current_cookie_count)
+        last_time_products_checked = datetime.now()
+
     # print(current_cookie_count)
 
     # available_products = driver.find_elements(By.CSS_SELECTOR, value=".product.unlocked.enabled")
